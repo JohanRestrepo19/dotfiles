6ae12940fb29774fce3a1e214f02b8f1469578f2
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2024-07-21 18:08:46 -0500

    feat(wezterm): add new terminal configurations.

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font = wezterm.font("MesloLGS Nerd Font Mono")
+config.font_size = 14
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
