8c39fd1ad4f576b7087092f8645331f2e80d1e87
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2025-02-15 21:33:40 -0500

    feat(neovim): add snacks plugins.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,8 +5,4 @@
 -- ██║██║ ╚████║██║   ██║██╗███████╗╚██████╔╝██║  ██║
 -- ╚═╝╚═╝  ╚═══╝╚═╝   ╚═╝╚═╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
 
--- [ ] TODO:Set maximun size to status line git branch name component.
--- [ ] TODO:Try mini-indentline.
--- [ ] TODO:Try snacks.nvim for better statuscolumn.
-
 require("nighteye")
---
+++ .config/nvim/lua/nighteye/plugins/snacks.lua
@@ -0,0 +1,31 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    indent = {
+      enabled = false,
+      char = "┊",
+    },
+    statuscolumn = { enabled = true },
+  },
+
+  config = function()
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>il", function()
+      local snacks = require("snacks")
+
+      if snacks.indent.enabled == true then
+        return snacks.indent.disable()
+      end
+
+      if snacks.indent.enabled == false then
+        return snacks.indent.enable()
+      end
+
+    end, opts)
+  end,
+}
