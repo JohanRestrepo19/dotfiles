f1e3537c3a68a0ab119c7c8e423587d330ca244b
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2025-02-15 20:39:15 -0500

    chore(terminals): update terminals configuration.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,15 +2,15 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = "Kanagawa Dragon (Gogh)"
+config.color_scheme = "Catppuccin Mocha"
 
 config.font = wezterm.font("JetBrains Mono Nerd Font")
--- config.font = wezterm.font("MesloLGS Nerd Font")
+-- config.font = wezterm.font("UbuntuSansMonoNerdFont")
 config.font_size = 11.5
 
 config.enable_tab_bar = false
 
-config.window_background_opacity = 0.92
+config.window_background_opacity = 0.95
 
 config.window_padding = {
   left = 0,
@@ -22,8 +22,8 @@ config.window_padding = {
 config.initial_cols = 97
 config.initial_rows = 25
 
-config.enable_wayland = true
-config.window_decorations = "RESIZE"
+config.enable_wayland = false
+config.window_decorations = "TITLE | RESIZE"
 config.adjust_window_size_when_changing_font_size = false
 
 return config
